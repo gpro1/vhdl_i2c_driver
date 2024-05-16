f1274bd46588361d3549f09d0b7e3fb35477cc66
gpro1 <gpro1@rogers.com>
2024-05-16 17:10:32 -0400

    Updated project tcl script

--- i2c_driver.tcl
+++ i2c_driver.tcl
@@ -14,7 +14,7 @@
 
 # Quartus Prime: Generate Tcl File for Project
 # File: i2c_driver.tcl
-# Generated on: Fri May 10 15:55:08 2024
+# Generated on: Thu May 16 17:08:58 2024
 
 # Load Quartus Prime Tcl Project package
 package require ::quartus::project
@@ -50,7 +50,23 @@ if {$make_assignments} {
 	set_global_assignment -name MAX_CORE_JUNCTION_TEMP 85
 	set_global_assignment -name ERROR_CHECK_FREQUENCY_DIVISOR 1
 	set_global_assignment -name NOMINAL_CORE_SUPPLY_VOLTAGE 1.2V
-	set_global_assignment -name VHDL_FILE i2c_driver.vhd
+	set_global_assignment -name VHDL_FILE rtl/i2c_driver.vhd
+	set_global_assignment -name POWER_PRESET_COOLING_SOLUTION "23 MM HEAT SINK WITH 200 LFPM AIRFLOW"
+	set_global_assignment -name POWER_BOARD_THERMAL_MODEL "NONE (CONSERVATIVE)"
+	set_global_assignment -name PARTITION_NETLIST_TYPE SOURCE -section_id Top
+	set_global_assignment -name PARTITION_FITTER_PRESERVATION_LEVEL PLACEMENT_AND_ROUTING -section_id Top
+	set_global_assignment -name PARTITION_COLOR 16764057 -section_id Top
+	set_global_assignment -name EDA_SIMULATION_TOOL "ModelSim-Altera (VHDL)"
+	set_global_assignment -name EDA_TIME_SCALE "1 ps" -section_id eda_simulation
+	set_global_assignment -name EDA_OUTPUT_DATA_FORMAT VHDL -section_id eda_simulation
+	set_global_assignment -name VHDL_FILE i2c_driver_testbench.vhd
+	set_global_assignment -name EDA_TEST_BENCH_ENABLE_STATUS TEST_BENCH_MODE -section_id eda_simulation
+	set_global_assignment -name EDA_NATIVELINK_SIMULATION_TEST_BENCH i2c_driver_testbench -section_id eda_simulation
+	set_global_assignment -name EDA_TEST_BENCH_NAME i2c_driver_testbench -section_id eda_simulation
+	set_global_assignment -name EDA_DESIGN_INSTANCE_NAME NA -section_id i2c_driver_testbench
+	set_global_assignment -name EDA_TEST_BENCH_MODULE_NAME i2c_driver_testbench -section_id i2c_driver_testbench
+	set_global_assignment -name EDA_TEST_BENCH_FILE i2c_driver_testbench.vhd -section_id i2c_driver_testbench
+	set_instance_assignment -name PARTITION_HIERARCHY root_partition -to | -section_id Top
 
 	# Commit assignments
 	export_assignments

81007d17870fd7ce3b1676d9e5574b704a62a252
gpro1 <gpro1@rogers.com>
2024-05-28 14:42:56 -0400

    added SDA/SDL clk gen process

--- rtl/i2c_driver_oversampled.vhd
+++ rtl/i2c_driver_oversampled.vhd
@@ -45,7 +45,66 @@ end entity;
 
 architecture rtl of i2c_driver_oversampled is
 
+	constant C_QUARTER_CLK_CNT : integer := (G_INPUT_CLOCK / G_BUS_CLOCK) / 4;
+
+	signal r_sda_clk 				: std_logic := '0'; --Used to register data on SDA
+	signal r_scl_clk 				: std_logic := '0'; --Used to drive SCL
+	
+	--datatype and state register for I2C state machine
+	type 	 t_i2c_state is (idle, start, addr_transmit, data_transmit, data_read, ack, stop);
+	signal r_state 				: t_i2c_state := idle;
+
+	
+	signal r_en_1 					: std_logic := '0'; --Previously registered i_en signal (1 clock cycle)
+	
+	signal r_bit_cnt				: unsigned(3 downto 0) := (others => '0'); --Counter for transmitted/received bits
+	signal r_rcv_data				: unsigned(7 downto 0) := (others => '0'); --Receive data buffer
+
+	signal r_bus_addr_rw 		: unsigned(7 downto 0) := (others => '0'); --I2C address buffer
+	signal r_bus_data 			: unsigned(7 downto 0) := (others => '0'); --I2C transmit data buffer
+	
+	signal r_clk_cnt				: integer range 0 to C_QUARTER_CLK_CNT * 4 := 0; 
+
 begin
+
+INTERNAL_CLOCK_GEN : process (i_clk)
+begin
+	
+	if rising_edge(i_clk) then
+	
+		if r_clk_cnt = C_QUARTER_CLK_CNT - 1 then
+		
+			r_sda_clk <= '1';
+			r_scl_clk <= '0';
+			r_clk_cnt <= r_clk_cnt + 1;	
+		
+		elsif r_clk_cnt = (2 * C_QUARTER_CLK_CNT) - 1 then
+		
+			r_sda_clk <= '1';
+			r_scl_clk <= '1';
+			r_clk_cnt <= r_clk_cnt + 1;
+		
+		elsif r_clk_cnt = (3 * C_QUARTER_CLK_CNT) - 1 then
+		
+			r_sda_clk <= '0';
+			r_scl_clk <= '1';
+			r_clk_cnt <= r_clk_cnt + 1;
+		
+		elsif r_clk_cnt = (4 * C_QUARTER_CLK_CNT) - 1 then
+		
+			r_sda_clk <= '0';
+			r_scl_clk <= '0';
+			r_clk_cnt <= 0;
+		
+		else
+			
+			r_clk_cnt <= r_clk_cnt + 1;
+		
+		end if;
+	
+	end if;
+	
+end process;
 
 
 end rtl;
